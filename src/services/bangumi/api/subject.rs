79df417deb3778cc1987c4a9c90dc33b2471e056
hexsix <hexsixology@gmail.com>
2026-01-04 11:41:20 +0800

    feat: bangumi get subject impl

--- src/services/bangumi/api/subject.rs
+++ src/services/bangumi/api/subject.rs
@@ -1,8 +1,8 @@
 use crate::services::bangumi::{BangumiClient, schemas::*};
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 impl BangumiClient {
-    pub async fn get_subject(&self, subject_id: i32, _redirect_count: i32) -> Result<Subject> {
+    pub async fn get_subject(&self, subject_id: i32) -> Result<Subject> {
         /*
             获取条目详细信息
 
@@ -10,15 +10,64 @@ impl BangumiClient {
 
         Args:
             subject_id: 条目ID
-            _redirect_count: 内部重定向计数，用于防止无限重定向
 
         Returns:
             Subject: 条目详细信息
-
-        Raises:
-            httpx.HTTPStatusError: 当API返回错误状态码时
-            ValueError: 当响应数据解析失败或重定向次数过多时
         */
-        todo!()
+        let url = self
+            .base_url
+            .join(&format!("/v0/subjects/{}", subject_id))
+            .unwrap();
+
+        let response = self
+            .client
+            .get(url.as_str())
+            .send()
+            .await
+            .context("发送请求失败")?;
+
+        if !response.status().is_success() {
+            anyhow::bail!("API 返回错误状态码: {}, URL: {}", response.status(), url);
+        }
+
+        let subject = response
+            .json::<Subject>()
+            .await
+            .context("解析响应 JSON 失败")?;
+
+        Ok(subject)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_subject() {
+        let client = BangumiClient::new();
+        let subject = client.get_subject(400602).await.unwrap();
+
+        assert_eq!(subject.id, 400602);
+        assert_eq!(subject.name_cn, Some("葬送的芙莉莲".to_string()));
+        assert!(subject.rating.is_some());
+    }
+
+    #[tokio::test]
+    async fn test_bad_subject_not_found() {
+        let client = BangumiClient::new();
+        let subject = client.get_subject(999999999).await;
+
+        assert!(subject.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_get_subject_redirect() {
+        let client = BangumiClient::new();
+        let subject = client.get_subject(141079).await.unwrap();
+
+        assert_eq!(subject.id, 104906);
+        assert_eq!(subject.name_cn, Some("境界触发者".to_string()));
+        assert!(subject.rating.is_some());
     }
 }
--- src/services/bangumi/client.rs
+++ src/services/bangumi/client.rs
@@ -1,20 +1,39 @@
-use reqwest::Client;
+use reqwest::{Client, Url, header};
+use std::sync::Arc;
 
 const BANGUMI_API_BASE: &str = "https://api.bgm.tv";
 const USER_AGENT: &str = "rinshankaiho.fun (https://github.com/hexsix/bgm-rank-api)";
 
 pub struct BangumiClient {
     pub(super) client: Client,
-    pub(super) base_url: String,
-    pub(super) ua: String,
+    pub(super) base_url: Arc<Url>,
 }
 
 impl BangumiClient {
     pub fn new() -> Self {
+        let token = std::env::var("BGM_TOKEN").ok();
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            header::USER_AGENT,
+            header::HeaderValue::from_static(USER_AGENT),
+        );
+        if let Some(token) = token {
+            let auth_value = header::HeaderValue::from_str(&format!("Bearer {}", token));
+            if let Ok(auth_value) = auth_value {
+                headers.insert(header::AUTHORIZATION, auth_value);
+            }
+        }
+
+        let client = Client::builder()
+            .redirect(reqwest::redirect::Policy::limited(5))
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
         Self {
-            client: Client::new(),
-            base_url: BANGUMI_API_BASE.to_string(),
-            ua: USER_AGENT.to_string(),
+            client: client,
+            base_url: Arc::from(reqwest::Url::parse(BANGUMI_API_BASE).unwrap()),
         }
     }
 }
